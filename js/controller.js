d6c21f310a83930d72d682ebaef686d9b48b41d7
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-21 21:34:08 -0700

    added folder skeleton to project

---
+++ js/controller.js
@@ -0,0 +1,6 @@
+angular.module('miniRouting')
+  .controller('miniRoutingController', function($scope,routingService) {
+
+
+
+  })
---
+++ js/service.js
@@ -0,0 +1,6 @@
+angular.module('miniRouting')
+  .service('miniRoutingService', function() {
+
+
+
+  })
